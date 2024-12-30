5163dce5930702c4eccfa1d55414e4b25251c9c5
Kali123411 <64617127+Kali123411@users.noreply.github.com>
2024-12-29 19:28:10 -0700

    Update index.ts

--- src/treasury/index.ts
+++ src/treasury/index.ts
@@ -1,5 +1,5 @@
 import { EventEmitter } from 'events'
-import { PrivateKey, UtxoProcessor, UtxoContext, type RpcClient, type IPaymentOutput, createTransactions } from "../../wasm/kaspa"
+import { PrivateKey, UtxoProcessor, UtxoContext, type RpcClient, type IPaymentOutput, createTransactions } from "../../kaspa-wasm"
 
 export default class Treasury extends EventEmitter {
   privateKey: PrivateKey
